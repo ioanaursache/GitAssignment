d37367fb1e915db8ddf3158ebb0e5d7f7b612b43
ioanaursache <51614354+ioanaursache@users.noreply.github.com>
2020-03-23 14:52:40 +0200

    Create TestGit.java

---
+++ TestGit.java
@@ -0,0 +1,8 @@
+package tema2;
+
+public class TestGit {
+	public static void main(String args[])
+	{
+		System.out.println("Hello Git! Tema proiectului de licenta: Aplicatie informatica pentru gesiunea resurselor umane.");
+	}
+}
