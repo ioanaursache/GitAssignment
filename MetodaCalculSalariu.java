1f1fe6a7050de19a8843640f8d631ab7bd160675
ioanaursache <51614354+ioanaursache@users.noreply.github.com>
2020-03-23 14:55:00 +0200

    Create MetodaCalculSalariu.java

---
+++ MetodaCalculSalariu.java
@@ -0,0 +1,22 @@
+package tema2;
+
+import java.beans.ExceptionListener;
+
+public abstract class MetodaCalculSalariu implements ExceptionListener {
+	
+	public static float CalculeazaSalariu(Angajat angajat) throws Exception 
+	{
+		float salariul=0;
+		if(angajat.getOreLucrate()<=0)
+		{
+			throw new Exception("Angajatul " + angajat.getNumeAngajat()+ "este in concediu.");
+		}
+			
+		
+		salariul= salariul + (angajat.getPlataOra())  * (angajat.getOreLucrate()) ;
+	
+		return salariul; 
+	}
+
+
+}
